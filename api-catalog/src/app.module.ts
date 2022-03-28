5c57f6ab76d28f6e9be742df386d8b4468acfec3
Oleksandr Kushnarenko <oleksandr_kushnarenko@epam.com>
2022-03-28 17:51:40 -0600

    Init category and product resources

--- api-catalog/src/app.module.ts
+++ api-catalog/src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CategoryModule } from './category/category.module';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [],
+  imports: [CategoryModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
