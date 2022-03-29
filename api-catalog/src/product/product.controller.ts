3b885b7db2327f8bae04ad34d047c38f80dbe24e
Oleksandr Kushnarenko <oleksandr_kushnarenko@epam.com>
2022-03-29 13:45:57 -0600

    Add swagger page

--- api-catalog/src/product/product.controller.ts
+++ api-catalog/src/product/product.controller.ts
@@ -1,17 +1,19 @@
 import {
-  Controller,
-  Get,
-  Post,
   Body,
-  Patch,
-  Param,
+  Controller,
   Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
 } from '@nestjs/common';
-import { ProductService } from './product.service';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { ProductService } from './product.service';
 
 @Controller('product')
+@ApiTags('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
