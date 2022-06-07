76a1bfaa0455da5f00cc53580fbae3889eb6abfc
Oleksandr Kushnarenko <oleksandr_kushnarenko@epam.com>
2022-06-06 20:25:04 -0500

    Add db configs

--- api-catalog/src/product/product.service.ts
+++ api-catalog/src/product/product.service.ts
@@ -1,19 +1,51 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import * as AWS from 'aws-sdk';
+import { v4 as uuid } from 'uuid';
+
+const dynamoDB = new AWS.DynamoDB.DocumentClient();
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+  async create(createProductDto: CreateProductDto) {
+    const product = {
+      id: uuid(),
+      ...createProductDto,
+      createdAt: new Date().toISOString(),
+      updatedAt: new Date().toISOString(),
+    };
+    try {
+      await dynamoDB
+        .put({
+          TableName: process.env.PRODUCTS_TABLE_NAME,
+          Item: product,
+        })
+        .promise();
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+    return product;
   }
 
   findAll() {
     return `This action returns all product`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    let product;
+    try {
+      const result = await dynamoDB
+        .get({
+          TableName: process.env.PRODUCTS_TABLE_NAME,
+          Key: { id },
+        })
+        .promise();
+      product = result.Item;
+    } catch (error) {
+      throw new InternalServerErrorException(error);
+    }
+    return product;
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
